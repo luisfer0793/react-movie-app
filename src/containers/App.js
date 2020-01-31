262926a490fcef0a087058ef4ebf3309a989d7d3
Luis Fernando Jimenez Lopez <luisfer.0793@gmail.com>
2020-01-31 13:31:05 -0600

    refractor/App - Improved fetching

--- src/containers/App.js
+++ src/containers/App.js
@@ -12,18 +12,28 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            searchText: '',
             movies: [],
             categories: [
                 { id: 43873, name: 'Infantiles' },
                 { id: 43861, name: 'Acción' },
                 { id: 43864, name: 'Ciencia Ficción' },
             ],
-            searchText: '',
-            url: 'https://mfwkweb-api.clarovideo.net/services/content/list?quantity=40&order_way=DESC&order_id=200&level_id=GPS&from=0&node_id=43873&region=mexicoapi_version=v5.86&authpn=webclient&authpt=tfg1h3j4k6fd7&format=json&region=mexico&device_id=web&device_category=web&device_model=web&device_type=web&device_manufacturer=generic&HKS=9s5hqq76r3g6sg4jb90l38us52'
+            url: 'https://mfwkweb-api.clarovideo.net/services/content/list?device_id=web&device_category=web&device_model=web&device_type=web&format=json&device_manufacturer=generic&authpn=webclient&authpt=tfg1h3j4k6fd7&api_version=v5.89&region=mexico&HKS=la3krjfckhqh6cd5njgcurfbm1&quantity=40&order_way=DESC&order_id=200&level_id=GPS&from=0&node_id=43861'
         };
     }
     
     componentDidMount() {
+        this.fetchData();
+    }
+    
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevState.url !== this.state.url) {
+            this.fetchData();
+        }
+    }
+    
+    fetchData() {
         fetch(this.state.url)
             .then(response => response.json())
             .then(data => this.setState({movies: data.response.groups}))
@@ -36,9 +46,12 @@ class App extends Component {
         });
     }
     
-    changeCategoryHandler = event => {
+    changeCategoryHandler = () => {
+        const baseURL = `https://mfwkweb-api.clarovideo.net/services/content/list?`;
+        const params = new URLSearchParams('quantity=40&order_way=DESC&order_id=200&level_id=GPS&from=0&node_id=43861&region=mexicoapi_version=v5.86&authpn=webclient&authpt=tfg1h3j4k6fd7&format=json&region=mexico&device_id=web&device_category=web&device_model=web&device_type=web&device_manufacturer=generic&HKS=9s5hqq76r3g6sg4jb90l38us52');
+        params.set('node_id', '43873');
         this.setState({
-        
+           url: (baseURL + params.toString())
         });
     }
     
@@ -55,6 +68,7 @@ class App extends Component {
                     <MenuItemList categories={this.state.categories}/>
                 </nav>
                 <div className="app-movies-container">
+                    <button onClick={this.changeCategoryHandler}>Click</button>
                     <MovieList movies={filteredMovies}/>
                 </div>
             </div>
