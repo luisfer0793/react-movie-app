c18dfe0aa2c27a25405869723d36abb7917bc3c4
Luis Fernando Jimenez Lopez <luisfer.0793@gmail.com>
2020-02-04 13:59:19 -0600

    Updates

---
+++ precache-manifest.9f2c8491e61edfffe295bbb6da3586dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a4a4214e3ab3c5e31da78bc9789578",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "d39f6b406e230c0cc738",
+    "url": "/react-movie-app/static/css/main.6e1cde1c.chunk.css"
+  },
+  {
+    "revision": "75e383822adfa8ee2594",
+    "url": "/react-movie-app/static/js/2.76ae41ab.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-movie-app/static/js/2.76ae41ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d39f6b406e230c0cc738",
+    "url": "/react-movie-app/static/js/main.a6ea6c61.chunk.js"
+  },
+  {
+    "revision": "ee30f8a00c3886408ea0",
+    "url": "/react-movie-app/static/js/runtime-main.23d872c2.js"
+  },
+  {
+    "revision": "7270ae8708fd78e36ae732ff5dc07211",
+    "url": "/react-movie-app/static/media/logo.7270ae87.svg"
+  }
+]);
